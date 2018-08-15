a604b5cdd36bf4380ceda1f268f399af6fab2af4
Jack Paw <42397276+unixbadass@users.noreply.github.com>
2018-08-15 06:21:05 +0200

    Update addtask.php

--- addtask.php
+++ addtask.php
@@ -3,13 +3,13 @@ require("api/api.php");
 // usage: localhost/addtask.php?a=some_data&b=some_data
 
 if(isset($_GET['a']) && $_GET['b']) {
-  
-if(empty($a)) { die(); }
-if(empty($b)) { die(); }
 
 $a = $_GET['a'];
 $b = $_GET['b'];
-
+  
+if(empty($a)) { die(); }
+if(empty($b)) { die(); }
+  
 send_request( $a, $b );
 
 } else { die(); }
